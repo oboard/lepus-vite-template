879b7875233c3ea14d97fd86b6df3ca97f7fbd09
oboard <oboard@outlook.com>
2026-04-09 12:10:07 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import './style.css'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="https://github.com/lepus-apps/lepus" target="_blank">
+      <img class="logo" alt="Lepus logo" />
+    </a>
+    <a href="https://moonbitlang.cn/" target="_blank">
+      <img class="logo vanilla" alt="MoonBit logo" />
+    </a>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <h1>Lepus + MoonBit + Vite</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Lepus and MoonBit and Vite logos to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
