8150d7457b3207fdb9a63e0fd2629bbe2617ecd5
oboard <oboard@outlook.com>
2026-04-12 16:14:36 +0800

    feat: icons

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import './style.css'
 import viteLogo from '/vite.svg'
+import moonbitLogo from '/moonbit.png'
+import lepusLogo from '/lepus.svg'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://github.com/lepus-apps/lepus" target="_blank">
-      <img class="logo" alt="Lepus logo" />
+      <img src="${lepusLogo}" class="logo" alt="Lepus logo" />
     </a>
     <a href="https://moonbitlang.cn/" target="_blank">
-      <img class="logo vanilla" alt="MoonBit logo" />
+      <img src="${moonbitLogo}" class="logo vanilla" alt="MoonBit logo" />
     </a>
     <a href="https://vite.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
